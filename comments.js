389b587b3ef926c1c9ff6405d0ea830d35895174
Krish Koria <123854877+KrishKoria@users.noreply.github.com>
2024-06-10 13:59:02 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+//create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'comments.json');
+
+//parse JSON
+app.use(bodyParser.json());
+
+//get comments
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    if(err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+//add comment
+app.post('/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    if(err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
+    }
+})});
